fcbcc02d62e183ba2fb028f0edc7b401ca430c58
Salmaan Saeed <salsaeed999@gmail.com>
2025-07-04 22:55:32 -0400

    changed the wording for these sentences

--- manage_cluster.bash
+++ manage_cluster.bash
@@ -9,7 +9,7 @@ case "$1" in
         kubectl apply -f configmaps/
         kubectl apply -f namespaces/
         kubectl apply -f secrets/mongodb-secret.yaml
-        echo -e "\nApply all the configurations to the resources for the cluster."
+        echo -e "\nApply all the configurations to resources for the cluster."
         ;;
     "delete-all")
         kubectl delete deployments --all
@@ -20,7 +20,7 @@ case "$1" in
         ;;
     *)
         echo "start-minikube            start a minikube cluster using Docker"
-        echo "apply-all                 apply all the configurations to the resources for the cluster"
+        echo "apply-all                 apply all the configurations to resources for the cluster"
         echo "delete-all                delete all the resources of the cluster"
         ;;
 esac
