c309f9e7df1a670df9575dc2f018c522f3f4ffcc
Salmaan Saeed <salsaeed999@gmail.com>
2025-07-22 15:51:59 -0400

    Updated the manage_cluster.bash script. Added the create-new-minikube, create-secrets, install-dependencies flags.

--- manage_cluster.bash
+++ manage_cluster.bash
@@ -2,6 +2,9 @@
 
 case "$1" in
     "start-minikube")
+        minikube start --driver=docker --cpus=4 --memory=4g --disk-size=20g --cni=cilium --namespace=development
+        ;;
+    "create-new-minikube")
         minikube start --driver=docker --cpus=4 --memory=4g --disk-size=20g --cni=cilium
         ;;
     "apply-all")
@@ -15,6 +18,10 @@ case "$1" in
         kubectl apply -f statefulsets/
         kubectl apply -f cronjobs/
         kubectl apply -f pods/
+        kubectl apply -f limitranges/
+        kubectl apply -f networkpolicies/
+        kubectl apply -f priorityclasses/
+        kubectl apply -f resourcequotas/
         echo -e "\nApply all the configurations to resources for the cluster."
         ;;
     "delete-all")
@@ -27,6 +34,10 @@ case "$1" in
         kubectl delete statefulsets --all --force --grace-period=0
         kubectl delete cronjobs --all --force --grace-period=0
         kubectl delete pods --all --force --grace-period=0
+        kubectl delete limitranges --all --force --grace-period=0
+        kubectl delete networkpolicies --all --force --grace-period=0
+        kubectl delete priorityclasses --all --force --grace-period=0
+        kubectl delete resourcequotas --all --force --grace-period=0
         echo -e "\nDelete all the resources of the cluster."
         ;;
     "switch-to-dev")
@@ -35,17 +46,45 @@ case "$1" in
         ;;
     "create-namespaces")
         kubectl create -f namespaces/
+        kubectl config set-context dev --namespace=development --cluster=minikube --user=minikube
+        kubectl config set-context prod --namespace=production --cluster=minikube --user=minikube
         ;;
-    "create-secrets-from-file")
+    "create-secrets")
+        bash -c "openssl rand -base64 756 > keyfiles/mongodb-keyfile"
         kubectl create secret generic mongodb-keyfile --from-file=keyfiles/mongodb-keyfile
+        openssl genrsa -out tls_certificate/ca.key 2048
+        openssl req -x509 -new -nodes -days 365 -key tls_certificate/ca.key -out tls_certificate/ca.crt -subj "/CN=nginx.demo.io/O=Spirit Technologies/OU=Spirit Cloud"
+        kubectl create secret tls tls-secret --key tls_certificate/ca.key --cert tls_certificate/ca.crt
+        ;;
+    "install-dependencies")
+        # install ingress
+        helm upgrade --install ingress-nginx ingress-nginx \
+        --repo https://kubernetes.github.io/ingress-nginx \
+        --namespace ingress-nginx --create-namespace
+        minikube addons enable ingress
+
+        # install metrics server
+        kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+        # install prometheus and grafana
+        helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+        helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack
+
+        # install prometheus mongodb exporter
+        helm install prometheus-mongodb-exporter prometheus-community/prometheus-mongodb-exporter -f mongodb-exporter/values.yaml
+
+        # install cilium
+        cilium install
         ;;
     *)
-        echo "start-minikube                start a minikube cluster using Docker"
+        echo "start-minikube                start a minikube cluster using Docker and with the development namespace"
+        echo "create-new-minikube           create a new minikube cluster using Docker"
         echo "apply-all                     apply all the configurations to resources for the cluster"
         echo "delete-all                    delete all the resources of the cluster"
         echo "switch-to-dev                 switch to the development namespace"
         echo "create-namespaces             create all namespaces for the cluster"
-        echo "create-secrets-from-file      create all secrets from a file"
+        echo "create-secrets                create all secrets for the cluster"
+        echo "install-dependencies          install all dependencies for the cluster"
         ;;
 esac
 
